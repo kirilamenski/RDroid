daeed4ece7febc7727cb17cfa8c1c6fd55ca0e9a
KirilaMenski <bv_1514@mail.ru>
2018-12-28 16:06:37 +0300

    todo send adb command in one process stream

---
+++ src/main/java/com/ansgar/rdroidpc/commands/DeviceConnector.java
@@ -0,0 +1,53 @@
+package com.ansgar.rdroidpc.commands;
+
+import com.ansgar.rdoidpc.entities.Device;
+
+import java.io.*;
+import java.util.Locale;
+
+// TODO Find a path to solve problem with send commands in one process stream
+public class DeviceConnector {
+
+    private Device device;
+//    private Process process;
+//    private BufferedWriter writer;
+//    private BufferedReader reader;
+
+    public DeviceConnector(Device device) {
+        this.device = device;
+//        try {
+//            this.process = Runtime.getRuntime()
+//                    .exec(String.format(Locale.ENGLISH, "adb -s %s shell", device.getDeviceId()));
+//            outputStream = process.getOutputStream();
+//            this.process = new ProcessBuilder()
+//                    .command(String.format(Locale.ENGLISH, "adb -s %s shell", device.getDeviceId()))
+//                    .start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void execute(String command) throws IOException {
+        System.out.println(command);
+        Process process = Runtime.getRuntime()
+                .exec(String.format(Locale.ENGLISH, "adb -s %s shell", device.getDeviceId()));
+        if (process != null) {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            try {
+                writer.append(command);
+                writer.flush();
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+
+}
