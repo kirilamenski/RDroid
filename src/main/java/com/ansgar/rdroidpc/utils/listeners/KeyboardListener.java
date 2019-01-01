fe42d209f641974c219f55910907e49129b201a6
KirilaMenski <bv_1514@mail.ru>
2019-01-01 19:19:15 +0300

    add multiple keyboard event (shortcuts)

--- src/main/java/com/ansgar/rdroidpc/utils/listeners/KeyboardListener.java
+++ src/main/java/com/ansgar/rdroidpc/utils/listeners/KeyboardListener.java
@@ -6,10 +6,12 @@ import com.ansgar.rdroidpc.ui.frames.VideoFrame;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashMap;
 
 public class KeyboardListener implements KeyListener {
 
     private VideoFrame frame;
+    private HashMap<Integer, Integer> pressedKey = new HashMap();
 
     public KeyboardListener(VideoFrame frame) {
         this.frame = frame;
@@ -21,16 +23,39 @@ public class KeyboardListener implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        pressedKey.put(e.getKeyCode(), e.getKeyCode());
+        System.out.println("Size: " + pressedKey.size() + ", " + e.getKeyCode());
+        if (pressedKey.size() > 1) {
+            checkForMultipleKey();
+            return;
+        }
+
         AdbKeyCode adbKeyCode = AdbKeyCode.Companion.getAdbKeyEvent(e);
         if (adbKeyCode != AdbKeyCode.KEYCODE_UNKNOWN && adbKeyCode.isChar()) {
             frame.getChimpDevice().type(String.valueOf(e.getKeyChar()));
         } else {
             frame.getChimpDevice().press(String.valueOf(adbKeyCode.getKeyCode()), TouchPressType.DOWN_AND_UP);
         }
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        pressedKey.remove(e.getKeyCode());
+    }
+
+    private void checkForMultipleKey() {
+        AdbKeyCode adbKeyCode = AdbKeyCode.KEYCODE_UNKNOWN;
+        if (pressedKey.containsKey(KeyEvent.VK_ALT) && pressedKey.containsKey(KeyEvent.VK_LEFT)) {
+            adbKeyCode = AdbKeyCode.KEYCODE_BACK;
+        } else if (pressedKey.containsKey(KeyEvent.VK_CONTROL) && pressedKey.containsKey(KeyEvent.VK_SHIFT) && pressedKey.containsKey(KeyEvent.VK_H)) {
+            adbKeyCode = AdbKeyCode.KEYCODE_HOME;
+        } else if (pressedKey.containsKey(KeyEvent.VK_CONTROL) && pressedKey.containsKey(KeyEvent.VK_SHIFT) && pressedKey.containsKey(KeyEvent.VK_L)) {
+            adbKeyCode = AdbKeyCode.KEYCODE_APP_SWITCH;
+        }
+        if (adbKeyCode != AdbKeyCode.KEYCODE_UNKNOWN) {
+            frame.getChimpDevice().press(String.valueOf(adbKeyCode.getKeyCode()), TouchPressType.DOWN_AND_UP);
+        }
     }
 
 }
