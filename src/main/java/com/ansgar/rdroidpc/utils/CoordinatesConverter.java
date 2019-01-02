f4b246d481219f21f0bb19e2429b502e12c4a45d
KirilaMenski <bv_1514@mail.ru>
2019-01-02 15:05:22 +0300

    refactor

--- src/main/java/com/ansgar/rdroidpc/utils/CoordinatesConverter.java
+++ src/main/java/com/ansgar/rdroidpc/utils/CoordinatesConverter.java
@@ -1,6 +1,6 @@
 package com.ansgar.rdroidpc.utils;
 
-public class Converter {
+public class CoordinatesConverter {
 
     public static float convertCoordinates(float originalHeight, float currentHeight, float coordinate) {
         return (originalHeight / currentHeight) * coordinate;
