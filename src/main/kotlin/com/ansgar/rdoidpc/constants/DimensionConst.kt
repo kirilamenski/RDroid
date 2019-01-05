be877ccc4a566de67d5b0d8eda9716b7c76b7ae6
KirilaMenski <bv_1514@mail.ru>
2019-01-05 19:39:07 +0300

    refactor changed window size

--- src/main/kotlin/com/ansgar/rdoidpc/constants/DimensionConst.kt
+++ src/main/kotlin/com/ansgar/rdoidpc/constants/DimensionConst.kt
@@ -3,11 +3,12 @@ package com.ansgar.rdoidpc.constants
 class DimensionConst {
 
     companion object {
-        const val MAIN_WINDOW_WIDTH = 640
-        const val MAIN_WINDOW_HEIGHT = 840
+        const val MAIN_WINDOW_WIDTH = 800
+        const val MAIN_WINDOW_HEIGHT = 420
         const val NAVIGATION_PANEL_HEIGHT = 40
         const val SCREEN_RATIO = 0.5625f
         const val DEFAULT_WIDTH = 720
+        const val DEVICE_CONTAINER_HEIGHT = 50
     }
 
 }
