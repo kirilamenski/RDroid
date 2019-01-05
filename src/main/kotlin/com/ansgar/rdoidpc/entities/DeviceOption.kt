819e26477a19da4071fcf7e71d9b7034c257961b
KirilaMenski <bv_1514@mail.ru>
2019-01-05 19:20:34 +0300

    add class with device options

---
+++ src/main/kotlin/com/ansgar/rdoidpc/entities/DeviceOption.kt
@@ -0,0 +1,18 @@
+package com.ansgar.rdoidpc.entities
+
+class DeviceOption(
+        var deviceId: String? = null,
+        var deviceName: String? = null,
+        var bitRate: Int = 4,
+        var width: Int = 720,
+        var height: Int = 1280) {
+
+    override fun toString(): String = "[" +
+            "deviceId: $deviceId, " +
+            "deviceName: $deviceName," +
+            " bitRate: $bitRate," +
+            " width: $width," +
+            " height: $height" +
+            "]"
+
+}
