69225aeb480ca7a13f731def0be096d03c67b425
KirilaMenski <bv_1514@mail.ru>
2019-01-05 19:37:09 +0300

    add device table headers name

--- src/main/kotlin/com/ansgar/rdoidpc/constants/StringConst.kt
+++ src/main/kotlin/com/ansgar/rdoidpc/constants/StringConst.kt
@@ -6,5 +6,6 @@ class StringConst {
         const val MENU_TITLE_FILE = "File"
         const val MENU_TITLE_HELP = "Help"
         const val MENU_SETTINGS = "Settings"
+        val DEVICES_CONTAINER_HEADER_NAMES = arrayOf("name", "size", "device id", "status", "")
     }
 }
