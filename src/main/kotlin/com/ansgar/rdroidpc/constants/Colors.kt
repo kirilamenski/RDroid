0e3f7fb3dcd3f15fdfc8c7867a16afbb81ac6d92
KirilaMenski <bv_1514@mail.ru>
2019-01-07 19:19:13 +0300

    add menu background color

--- src/main/kotlin/com/ansgar/rdroidpc/constants/Colors.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/constants/Colors.kt
@@ -3,5 +3,6 @@ package com.ansgar.rdroidpc.constants
 class Colors {
     companion object {
         const val MAIN_BACKGROUND_COLOR = "#3E3E3E"
+        const val MENU_BACKGROUND_COLOR = "#363636"
     }
 }
