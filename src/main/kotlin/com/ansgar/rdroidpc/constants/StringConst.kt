2ca6aa9fdfd2a7b8d75f044d7cc78925cd1e2476
KirilaMenski <bv_1514@mail.ru>
2019-01-07 19:20:43 +0300

    add menu names value

--- src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
@@ -6,6 +6,7 @@ class StringConst {
         const val MENU_TITLE_FILE = "File"
         const val MENU_TITLE_HELP = "Help"
         const val MENU_SETTINGS = "Settings"
+        var menuItems = arrayOf("File", arrayOf("Settings", "Exit"), "Help")
         val DEVICES_CONTAINER_HEADER_NAMES = arrayOf("name", "size", "device id", "status", "")
     }
 }
