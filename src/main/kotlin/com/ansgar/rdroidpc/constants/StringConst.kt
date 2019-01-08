eae568552ee154439054045bbb9d70bd51ebee02
KirilaMenski <bv_1514@mail.ru>
2019-01-08 09:46:55 +0300

    refactor remove table head names

--- src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
@@ -3,9 +3,6 @@ package com.ansgar.rdroidpc.constants
 class StringConst {
 
     companion object {
-        const val MENU_TITLE_FILE = "File"
-        const val MENU_TITLE_HELP = "Help"
-        const val MENU_SETTINGS = "Settings"
         var menuItems = arrayOf("File", arrayOf("Settings", "Exit"), "Help")
         val DEVICES_CONTAINER_HEADER_NAMES = arrayOf("name", "size", "device id", "status", "")
     }
