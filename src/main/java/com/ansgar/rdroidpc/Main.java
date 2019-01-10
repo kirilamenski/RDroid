ca535d2aba56cb80b264512c11a35520e0a026a8
KirilaMenski <bv_1514@mail.ru>
2019-01-10 22:42:39 +0300

    add initializing shared values class

--- src/main/java/com/ansgar/rdroidpc/Main.java
+++ src/main/java/com/ansgar/rdroidpc/Main.java
@@ -3,12 +3,18 @@ package com.ansgar.rdroidpc;
 import com.ansgar.rdroidpc.constants.DimensionConst;
 import com.ansgar.rdroidpc.constants.ProgrammInf;
 import com.ansgar.rdroidpc.ui.MainPanel;
+import com.ansgar.rdroidpc.utils.AppUiCustomizationUtil;
+import com.ansgar.rdroidpc.utils.SharedValues;
 
 import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        SharedValues.newInstance();
+        AppUiCustomizationUtil.customizeApp();
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setTitle(ProgrammInf.NAME + "/" + ProgrammInf.VERSION);
