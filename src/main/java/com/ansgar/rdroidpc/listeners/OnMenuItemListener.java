9a6d3bdb20fcd9568d22e8e63d69753afa56d470
KirilaMenski <bv_1514@mail.ru>
2019-01-10 22:43:46 +0300

    add settings listener

--- src/main/java/com/ansgar/rdroidpc/listeners/OnMenuItemListener.java
+++ src/main/java/com/ansgar/rdroidpc/listeners/OnMenuItemListener.java
@@ -4,4 +4,6 @@ public interface OnMenuItemListener {
 
     void onItemClicked(String name);
 
+    void onAdbPathChanged(String path);
+
 }
--- src/main/java/com/ansgar/rdroidpc/listeners/impl/MainPanelMenuListenerImpl.java
+++ src/main/java/com/ansgar/rdroidpc/listeners/impl/MainPanelMenuListenerImpl.java
@@ -2,9 +2,11 @@ package com.ansgar.rdroidpc.listeners.impl;
 
 import com.ansgar.rdroidpc.constants.DimensionConst;
 import com.ansgar.rdroidpc.constants.MenuItemsEnum;
+import com.ansgar.rdroidpc.constants.SharedValuesKey;
 import com.ansgar.rdroidpc.listeners.OnMenuItemListener;
 import com.ansgar.rdroidpc.ui.MainPanel;
 import com.ansgar.rdroidpc.ui.SettingsFrame;
+import com.ansgar.rdroidpc.utils.SharedValues;
 
 import java.awt.*;
 
@@ -25,14 +27,21 @@ public class MainPanelMenuListenerImpl implements OnMenuItemListener {
                 case SETTINGS:
                     Rectangle rectangle = new Rectangle(700, 100,
                             DimensionConst.SETTINGS_PANEL_WIDTH, DimensionConst.SETTINGS_PANEL_HEIGHT);
-                    new SettingsFrame(rectangle, MenuItemsEnum.SETTINGS.getValue());
+                    SettingsFrame settingsFrame = new SettingsFrame(rectangle, MenuItemsEnum.SETTINGS.getValue());
+                    settingsFrame.setListener(this);
                     break;
                 case EXIT:
-                    // TODO in exit block stop all opened connections and restart adb server
-                    panel.getAdbBackend().shutdown();
-                    System.exit(0);
+                    panel.closeDevicesConnections();
                     break;
             }
+        }
+    }
+
+    @Override
+    public void onAdbPathChanged(String path) {
+        if (!SharedValues.get(SharedValuesKey.ADB_PATH, "").equals(path)) {
+            SharedValues.put(SharedValuesKey.ADB_PATH, path);
+            panel.closeDevicesConnections();
         }
     }
 
