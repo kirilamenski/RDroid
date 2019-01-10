6040f31b3f0bec3eecfb7e5c4fd5f7b1d3941e6d
KirilaMenski <bv_1514@mail.ru>
2019-01-10 22:42:16 +0300

    add main customization class which set ui for all components

---
+++ src/main/java/com/ansgar/rdroidpc/utils/AppUiCustomizationUtil.java
@@ -0,0 +1,33 @@
+package com.ansgar.rdroidpc.utils;
+
+import com.ansgar.rdroidpc.constants.Colors;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AppUiCustomizationUtil {
+
+    public static void customizeApp() {
+        UIManager.put("ScrollPane.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+        UIManager.put("List.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+        UIManager.put("List.foreground", Color.WHITE);
+
+        UIManager.put("Panel.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+        UIManager.put("MenuItem.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+        UIManager.put("MenuItem.foreground", Color.WHITE);
+        UIManager.put("MenuBar.background", Color.decode(Colors.MENU_BACKGROUND_COLOR));
+        UIManager.put("Button.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+        UIManager.put("Button.foreground", Color.WHITE);
+        UIManager.put("ComboBox.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+        UIManager.put("ComboBox.foreground", Color.WHITE);
+
+        UIManager.put("TextField.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+        UIManager.put("TextField.foreground", Color.WHITE);
+        UIManager.put("Label.foreground", Color.WHITE);
+        UIManager.put("ToolBar.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+
+        UIManager.put("Viewport.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+        UIManager.put("Viewport.foreground", Color.WHITE);
+    }
+
+}
