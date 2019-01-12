db35fd0061397ba962a1176a360605a55ad9cfc9
KirilaMenski <bv_1514@mail.ru>
2019-01-12 22:33:52 +0300

    add window listener

--- src/main/java/com/ansgar/rdroidpc/ui/BasePanel.java
+++ src/main/java/com/ansgar/rdroidpc/ui/BasePanel.java
@@ -2,8 +2,12 @@ package com.ansgar.rdroidpc.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class BasePanel extends JPanel {
+public class BasePanel extends JPanel implements WindowListener {
+
+    protected JFrame frame;
 
     public BasePanel(Rectangle rectangle, String title) {
         setLayout(null);
@@ -11,14 +15,53 @@ public class BasePanel extends JPanel {
     }
 
     private void initFrame(Rectangle rectangle, String title) {
-        JFrame frame = new JFrame(title);
-        frame.add(this);
+        frame = new JFrame(title);
         frame.pack();
         frame.setVisible(true);
         frame.setFocusable(true);
         frame.setFocusTraversalKeysEnabled(false);
         frame.setResizable(false);
         frame.setBounds(rectangle);
+        frame.add(this);
+        frame.addWindowListener(this);
     }
 
+    protected void onCloseApp() {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        onCloseApp();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
 }
