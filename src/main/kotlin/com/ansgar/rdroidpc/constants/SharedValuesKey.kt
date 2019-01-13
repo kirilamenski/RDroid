8755a783296468fd51dff527f340cf502f07d77d
KirilaMenski <bv_1514@mail.ru>
2019-01-13 22:54:17 +0300

    add upload files key

--- src/main/kotlin/com/ansgar/rdroidpc/constants/SharedValuesKey.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/constants/SharedValuesKey.kt
@@ -3,5 +3,6 @@ package com.ansgar.rdroidpc.constants
 class SharedValuesKey {
     companion object {
         const val ADB_PATH = "adb_path"
+        const val UPLOAD_FOLDER = "upload_folder"
     }
 }
