0235b64dbbb6c9e05f3dd2e606a40ff4580a9588
KirilaMenski <bv_1514@mail.ru>
2019-02-02 17:19:48 +0300

    add new strings const

--- src/main/java/com/ansgar/rdroidpc/utils/StringUtils.java
+++ src/main/java/com/ansgar/rdroidpc/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.ansgar.rdroidpc.utils;
 
 import com.ansgar.rdroidpc.constants.AdbCommandEnum;
 import com.ansgar.rdroidpc.entities.Device;
+import com.ansgar.rdroidpc.entities.DeviceOption;
 
 import java.nio.charset.Charset;
 import java.util.Locale;
@@ -32,6 +33,32 @@ public class StringUtils {
 
         return stringBuilder.toString();
     }
+
+    public static String getScreenRecordCommand(DeviceOption deviceOption, int count) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(
+                String.format(
+                        Locale.ENGLISH,
+                        AdbCommandEnum.Companion.getCommandValue(AdbCommandEnum.ADB_SHELL),
+                        deviceOption.getDeviceId()
+                )
+        ).append(" ");
+        String option = String.format(
+                Locale.ENGLISH,
+                AdbCommandEnum.SCREEN_RECORD.getCommand(),
+                deviceOption.getBitRate(),
+                deviceOption.getHeight(),
+                deviceOption.getWidth()
+        );
+
+        for (int i = 0; i <= count; i++) {
+            stringBuilder.append(option);
+            if (i != count) stringBuilder.append("; ");
+        }
+
+        return stringBuilder.toString();
+    }
+
 
     public static String getEncodedString(char ch) {
         byte[] bytes = String.valueOf(ch).getBytes();
