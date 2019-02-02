db39aa0603a1ce363411a9ddec89feaf21f7ea3d
KirilaMenski <bv_1514@mail.ru>
2019-02-02 17:16:57 +0300

    add listener to get callback of device options

---
+++ src/main/java/com/ansgar/rdroidpc/listeners/OnDeviceOptionsListener.java
@@ -0,0 +1,9 @@
+package com.ansgar.rdroidpc.listeners;
+
+import com.ansgar.rdroidpc.entities.DeviceOption;
+
+public interface OnDeviceOptionsListener {
+
+    void onDeviceOptionsSelected(DeviceOption deviceOption);
+
+}
