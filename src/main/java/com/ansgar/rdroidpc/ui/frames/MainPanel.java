cbe3878a93a986c0026f5afcb60f6c8e2b7a5917
KirilaMenski <bv_1514@mail.ru>
2019-02-02 17:20:36 +0300

    add ability to run instance with device options

--- src/main/java/com/ansgar/rdroidpc/ui/frames/MainPanel.java
+++ src/main/java/com/ansgar/rdroidpc/ui/frames/MainPanel.java
@@ -1,16 +1,18 @@
-package com.ansgar.rdroidpc.ui;
+package com.ansgar.rdroidpc.ui.frames;
 
 import com.android.chimpchat.adb.AdbBackend;
 import com.ansgar.rdroidpc.constants.*;
 import com.ansgar.rdroidpc.entities.Device;
 import com.ansgar.rdroidpc.commands.CommandExecutor;
 import com.ansgar.rdroidpc.commands.ResponseParserUtil;
+import com.ansgar.rdroidpc.entities.DeviceOption;
+import com.ansgar.rdroidpc.listeners.OnDeviceOptionsListener;
 import com.ansgar.rdroidpc.listeners.impl.MainPanelMenuListenerImpl;
 import com.ansgar.rdroidpc.ui.components.DevicesContainer;
-import com.ansgar.rdroidpc.ui.frames.VideoFrame;
 import com.ansgar.rdroidpc.ui.components.menu.MenuBar;
 import com.ansgar.rdroidpc.utils.StringUtils;
 import com.ansgar.rdroidpc.listeners.OnVideoFrameListener;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -18,17 +20,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
-public class MainPanel extends BasePanel implements OnVideoFrameListener, DevicesContainer.OnItemClicked {
+public class MainPanel extends BasePanel
+        implements OnVideoFrameListener, DevicesContainer.OnItemClicked, OnDeviceOptionsListener {
 
     private MenuBar menuBar;
     private HashMap<String, VideoFrame> openedDevices;
     private List<Device> devices;
     private AdbBackend adbBackend;
+    private List<DeviceOption> deviceOptions;
 
     public MainPanel(Rectangle rectangle, String title) {
         super(rectangle, title);
         this.menuBar = new MenuBar();
         this.openedDevices = new HashMap<>();
+        this.deviceOptions = new ArrayList<>();
         this.adbBackend = new AdbBackend();
         this.devices = new ArrayList<>();
         setUpMainPanel();
@@ -103,7 +108,14 @@ public class MainPanel extends BasePanel implements OnVideoFrameListener, Device
         if (!openedDevices.containsKey(device.getDeviceId())) {
             VideoFrame videoFrame = new VideoFrame(device, adbBackend);
             videoFrame.setOnVideoFrameListener(MainPanel.this);
-            videoFrame.startNewThread(StringUtils.getScreenRecordCommand(device, 12, 45));
+
+            DeviceOption deviceOption = getDeviceOptions(device.getDeviceId());
+            if (deviceOption != null) {
+                // TODO when you try to set screen resolution bigger then original than you can get exception related with count params
+                videoFrame.startNewThread(StringUtils.getScreenRecordCommand(deviceOption, 45));
+            } else {
+                videoFrame.startNewThread(StringUtils.getScreenRecordCommand(device, 12, 45));
+            }
             openedDevices.put(device.getDeviceId(), videoFrame);
         } else {
             System.out.println("Device is already opened.");
@@ -112,7 +124,21 @@ public class MainPanel extends BasePanel implements OnVideoFrameListener, Device
 
     @Override
     public void onDeviceSettings(int position, Device device) {
+        Rectangle rectangle = new Rectangle(getRectangle().x + 200, getRectangle().y, 400, 400);
+        DeviceOptionsFrame frame = new DeviceOptionsFrame(device, rectangle, StringConst.DEVICE_OPTIONS);
+        frame.setListener(this);
+    }
 
+    @Override
+    public void onDeviceOptionsSelected(DeviceOption deviceOption) {
+        for (int i = 0; i < deviceOptions.size(); i++) {
+            DeviceOption dOp = deviceOptions.get(i);
+            if (dOp.getDeviceId() != null && dOp.getDeviceId().equals(deviceOption.getDeviceId())) {
+                deviceOptions.remove(i);
+                break;
+            }
+        }
+        deviceOptions.add(deviceOption);
     }
 
     public void closeDevicesConnections() {
@@ -132,6 +158,16 @@ public class MainPanel extends BasePanel implements OnVideoFrameListener, Device
         if (adbBackend != null) {
             adbBackend.shutdown();
         }
+    }
+
+    @Nullable
+    private DeviceOption getDeviceOptions(String deviceId) {
+        if (deviceOptions.size() > 0) {
+            for (DeviceOption deviceOption : deviceOptions) {
+                if (deviceId.equals(deviceOption.getDeviceId())) return deviceOption;
+            }
+        }
+        return null;
     }
 
 }
--- src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
@@ -7,6 +7,11 @@ class StringConst {
                 MenuItemsEnum.FILE.value, arrayOf(MenuItemsEnum.SETTINGS.value, MenuItemsEnum.EXIT.value),
                 MenuItemsEnum.HELP.value)
         val DEVICES_CONTAINER_HEADER_NAMES = arrayOf("name", "size", "device id", "status", "")
+        val SCREEN_RESOLUTION_ARRAY_LIST = arrayOf("540x960", "720x1280", "1080x1920", "1440x24560")
+        const val BIT_RATE_L = "Bit Rate"
+        const val SCREEN_RESOLUTION_L = "Screen Resolution"
         const val OK = "Ok"
+        const val CANCEL = "Cancel"
+        const val DEVICE_OPTIONS = "Device Options"
     }
 }
