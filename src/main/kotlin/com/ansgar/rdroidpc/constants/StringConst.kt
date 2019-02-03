20b8c63cdbb76778e9ab186f2e9f969209e84877
KirilaMenski <bv_1514@mail.ru>
2019-02-03 17:14:46 +0300

    add bit rate const values

--- src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
@@ -1,5 +1,7 @@
 package com.ansgar.rdroidpc.constants
 
+import org.jetbrains.annotations.NotNull
+
 class StringConst {
 
     companion object {
@@ -8,6 +10,11 @@ class StringConst {
                 MenuItemsEnum.HELP.value)
         val DEVICES_CONTAINER_HEADER_NAMES = arrayOf("name", "size", "device id", "status", "")
         val SCREEN_RESOLUTION_ARRAY_LIST = arrayOf("540x960", "720x1280", "1080x1920", "1440x24560")
+        val bitRates = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "11", "12", "13", "14", "15", "15", "16", "17", "18", "19", "20",
+                "21", "22", "24", "25", "26", "27", "28", "29", "30",
+                "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+                "41", "42", "43", "44", "45", "46", "48", "49", "50")
         const val BIT_RATE_L = "Bit Rate"
         const val SCREEN_RESOLUTION_L = "Screen Resolution"
         const val OK = "Ok"
