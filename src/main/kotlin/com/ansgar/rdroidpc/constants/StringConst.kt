491ceb7b7567bd70598caa89ee065bcdec10ce3c
KirilaMenski <bv_1514@mail.ru>
2019-02-04 17:38:38 +0300

    fix screen size value

--- src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
@@ -7,7 +7,7 @@ class StringConst {
                 MenuItemsEnum.FILE.value, arrayOf(MenuItemsEnum.SETTINGS.value, MenuItemsEnum.EXIT.value),
                 MenuItemsEnum.HELP.value)
         val deviceHeaderNames = arrayOf("name", "size", "device id", "status", "")
-        val defaultScreenSizes = arrayOf("540x960", "720x1280", "1080x1920", "1440x24560")
+        val defaultScreenSizes = arrayOf("540x960", "720x1280", "1080x1920", "1440x2560")
         val bitRates = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                 "11", "12", "13", "14", "15", "15", "16", "17", "18", "19", "20",
                 "21", "22", "24", "25", "26", "27", "28", "29", "30",
