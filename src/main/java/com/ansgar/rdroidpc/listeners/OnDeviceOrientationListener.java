d7fb0d1228ac614a3d7603ce6e8f1f8d52f5c50f
KirilaMenski <bv_1514@mail.ru>
2019-02-04 22:05:23 +0300

    add listener interface to get device orientation

---
+++ src/main/java/com/ansgar/rdroidpc/listeners/OnDeviceOrientationListener.java
@@ -0,0 +1,9 @@
+package com.ansgar.rdroidpc.listeners;
+
+import com.ansgar.rdroidpc.enums.OrientationEnum;
+
+public interface OnDeviceOrientationListener {
+
+    void onOrientationChanged(OrientationEnum orientation);
+
+}
