1e34ffed6347f9cdd8635fd28ba2f744d37adc7c
KirilaMenski <bv_1514@mail.ru>
2019-02-05 21:59:19 +0300

    fix check value for orientation enum

--- src/main/kotlin/com/ansgar/rdroidpc/enums/OrientationEnum.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/enums/OrientationEnum.kt
@@ -6,6 +6,13 @@ enum class OrientationEnum(val value: Int) {
 
     companion object {
         fun getFromValue(value: String): OrientationEnum? = OrientationEnum.values()
-                .lastOrNull { it.value == value.toInt() }
+                .lastOrNull {
+                    if (value.isNotEmpty() && Character.isDigit(value[0])) {
+                        it.value == value.toInt()
+                    } else {
+                        println(value)
+                        return null
+                    }
+                }
     }
 }
