11b4d48bd60adf147d84dd4f271442f30fcf681d
KirilaMenski <bv_1514@mail.ru>
2019-02-06 22:09:19 +0300

    chor renamed values

--- src/main/java/com/ansgar/rdroidpc/utils/DimensionUtils.java
+++ src/main/java/com/ansgar/rdroidpc/utils/DimensionUtils.java
@@ -2,8 +2,8 @@ package com.ansgar.rdroidpc.utils;
 
 public class DimensionUtils {
 
-    public static float convertCoordinates(float originalHeight, float currentHeight, float coordinate) {
-        return (originalHeight / currentHeight) * coordinate;
+    public static float convertCoordinates(float originalSize, float currentSize, float coordinate) {
+        return (originalSize / currentSize) * coordinate;
     }
 
 }
