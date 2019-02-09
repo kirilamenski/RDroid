c5cc8252aa4438761c502a0d9caa35a066a8b61f
KirilaMenski <bv_1514@mail.ru>
2019-02-09 17:35:57 +0300

    add class with actions panel listener

--- src/main/java/com/ansgar/rdroidpc/listeners/MainActionPanelsListener.java
+++ src/main/java/com/ansgar/rdroidpc/listeners/MainActionPanelsListener.java
@@ -1,4 +1,23 @@
 package com.ansgar.rdroidpc.listeners;
 
-public class MainActionPanelsListener {
+import com.ansgar.rdroidpc.ui.components.ButtonsPanel;
+import com.ansgar.rdroidpc.ui.frames.MainPanel;
+
+public class MainActionPanelsListener implements ButtonsPanel.OnButtonPanelListener {
+
+    private MainPanel panel;
+
+    public MainActionPanelsListener(MainPanel panel) {
+        this.panel = panel;
+    }
+
+    @Override
+    public void onActionItemClicked(int position) {
+        System.out.println("Pos: " + position);
+        switch (position) {
+            case 0:
+                panel.executeAdbDevices();
+                break;
+        }
+    }
 }
