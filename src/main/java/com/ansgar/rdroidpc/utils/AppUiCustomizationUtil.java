537fa87edae56fc1ae21d9a7334325fc0f96b065
KirilaMenski <bv_1514@mail.ru>
2019-02-19 17:31:22 +0300

    change caret color

--- src/main/java/com/ansgar/rdroidpc/utils/AppUiCustomizationUtil.java
+++ src/main/java/com/ansgar/rdroidpc/utils/AppUiCustomizationUtil.java
@@ -14,22 +14,30 @@ public class AppUiCustomizationUtil {
             e.printStackTrace();
         }
         UIManager.put("ScrollPane.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
+
         UIManager.put("List.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
         UIManager.put("List.foreground", Color.WHITE);
 
         UIManager.put("Panel.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
         UIManager.put("Panel.foreground", Color.WHITE);
+
         UIManager.put("MenuItem.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
         UIManager.put("MenuItem.foreground", Color.WHITE);
+
         UIManager.put("MenuBar.background", Color.decode(Colors.MENU_BACKGROUND_COLOR));
+
         UIManager.put("Button.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
         UIManager.put("Button.foreground", Color.WHITE);
+
         UIManager.put("ComboBox.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
         UIManager.put("ComboBox.foreground", Color.WHITE);
 
         UIManager.put("TextField.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
         UIManager.put("TextField.foreground", Color.WHITE);
+        UIManager.put("TextField.caretForeground", Color.WHITE);
+
         UIManager.put("Label.foreground", Color.WHITE);
+
         UIManager.put("ToolBar.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
 
         UIManager.put("Viewport.background", Color.decode(Colors.MAIN_BACKGROUND_COLOR));
