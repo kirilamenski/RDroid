ae6d8fb6c0f9c73e3cebb5f5792cec0dd2c7adfb
KirilaMenski <bv_1514@mail.ru>
2019-03-04 22:18:54 +0300

    chor

--- src/main/kotlin/com/ansgar/rdroidpc/entities/Option.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/entities/Option.kt
@@ -5,4 +5,5 @@ import java.io.Serializable
 class Option(
         var bitRate: Int = 4,
         var width: Int = 720,
-        var height: Int = 1280) : Serializable
+        var height: Int = 1280
+) : Serializable
