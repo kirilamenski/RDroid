8441007ac32f2c390020424a6b5a68e1b54c6f80
KirilaMenski <bv_1514@mail.ru>
2019-03-06 22:46:41 +0300

    add enum to store files names

---
+++ src/main/kotlin/com/ansgar/rdroidpc/entities/FilesEnum.kt
@@ -0,0 +1,11 @@
+package com.ansgar.rdroidpc.entities
+
+enum class FilesEnum(val value: String) {
+    // Directories
+    CACHE("cache"),
+
+    // Files
+    DEVICES("devices.txt"),
+    SETTINGS("settings.txt"),
+    LOG("log.txt"),
+}
