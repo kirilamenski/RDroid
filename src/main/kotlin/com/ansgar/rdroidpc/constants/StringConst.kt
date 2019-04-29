220e46e307e55dc8fa7c9c63ffe4f9a61633b1d9
KirilaMenski <bv_1514@mail.ru>
2019-04-29 10:32:21 +0300

    add new screen size

--- src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
+++ src/main/kotlin/com/ansgar/rdroidpc/constants/StringConst.kt
@@ -9,7 +9,7 @@ class StringConst {
                 MenuItemsEnum.FILE.value, arrayOf(MenuItemsEnum.SETTINGS.value, MenuItemsEnum.EXIT.value),
                 MenuItemsEnum.HELP.value, arrayOf(MenuItemsEnum.INFORMATION.value))
         val deviceHeaderNames = arrayOf("name", "size", "device id", "status", "")
-        val defaultScreenSizes = arrayOf("480x640", "540x960", "720x1280", "1080x1920", "1440x2560")
+        val defaultScreenSizes = arrayOf("480x640", "540x960", "720x1280", "1080x1920", "1080x2280", "1440x2560")
         val bitRates = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                 "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
                 "21", "22", "24", "25", "26", "27", "28", "29", "30",
