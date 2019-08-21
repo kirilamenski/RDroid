dc0d8eed2afe5308f846c0863c5998b39bd23927
KirilaMenski <bv_1514@mail.ru>
2019-08-21 12:54:49 +0300

    refactor use generic

--- src/main/java/com/ansgar/rdroidpc/utils/SharedValues.java
+++ src/main/java/com/ansgar/rdroidpc/utils/SharedValues.java
@@ -2,7 +2,7 @@ package com.ansgar.rdroidpc.utils;
 
 import java.util.HashMap;
 
-public class SharedValues<T> {
+public class SharedValues {
 
     private static SharedValues instance;
     private static HashMap<String, Object> sharedVal;
@@ -21,65 +21,17 @@ public class SharedValues<T> {
         sharedVal = new HashMap<>();
     }
 
-    public static void put(String key, int value) {
-        sharedVal.put(key, value);
-    }
-
-    public static void put(String key, double value) {
-        sharedVal.put(key, value);
-    }
-
-    public static void put(String key, float value) {
-        sharedVal.put(key, value);
-    }
-
-    public static void put(String key, String value) {
-        sharedVal.put(key, value);
-    }
-
-    public static void put(String key, boolean value) {
-        sharedVal.put(key, value);
-    }
-
-    public static void put(String key, long value) {
-        sharedVal.put(key, value);
-    }
-
     public static <T> void put(String key, T value) {
         sharedVal.put(key, value);
     }
 
-    public static int get(String key, int defaultValue) {
-        return (int) getValue(key, defaultValue);
-    }
-
-    public static double get(String key, double defaultValue) {
-        return (double) getValue(key, defaultValue);
-    }
-
-    public static float get(String key, float defaultValue) {
-        return (float) getValue(key, defaultValue);
-    }
-
-    public static String get(String key, String defaultValue) {
-        return (String) getValue(key, defaultValue);
-    }
-
-    public static boolean get(String key, boolean defaultValue) {
-        return (boolean) getValue(key, defaultValue);
-    }
-
-    public static long get(String key, long defaultValue) {
-        return (long) getValue(key, defaultValue);
-    }
-
-    public static <T> Object get(String key, T defaultValue) {
+    public static <T> T get(String key, T defaultValue) {
         return getValue(key, defaultValue);
     }
 
-    private static Object getValue(String key, Object defaultValue) {
+    private static <T> T getValue(String key, T defaultValue) {
         if (sharedVal != null && sharedVal.containsKey(key)) {
-            return sharedVal.get(key);
+            return (T) sharedVal.get(key);
         } else {
             return defaultValue;
         }
