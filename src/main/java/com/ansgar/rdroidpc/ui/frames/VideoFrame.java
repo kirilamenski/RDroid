97841635f1acb82d298807efa59ebaa418bfaaa4
KirilaMenski <bv_1514@mail.ru>
2019-08-29 13:00:40 +0300

    chore

--- src/main/java/com/ansgar/rdroidpc/ui/frames/VideoFrame.java
+++ src/main/java/com/ansgar/rdroidpc/ui/frames/VideoFrame.java
@@ -121,7 +121,6 @@ public class VideoFrame extends BasePanel<VideoFramePresenter> implements VideoF
         try {
             if (frameGrabber != null && frameGrabber.hasVideo()) {
                 frameGrabber.stop();
-                frameGrabber = null;
             }
         } catch (FrameGrabber.Exception e) {
             e.printStackTrace();
